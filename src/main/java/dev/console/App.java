68c0cbca99b467819f3cd2653683e2589813d211
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-05-06 11:38:30 +0200

    TP01

---
+++ src/main/java/dev/console/App.java
@@ -0,0 +1,21 @@
+package dev.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;
+
+public class App {
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+		new AppService().executer("valeur 1");
+
+	}
+
+}
